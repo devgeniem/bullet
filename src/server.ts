30a815ad171e8e90544115f74a4c49787589d104
Topi Latva-Salo <topi.latva-salo@geniem.com>
2018-11-29 19:19:15 +0200

    Add logging middleware

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import * as express from 'express';
+import * as morgan from 'morgan';
 
 const enviroment = process.env.NODE_ENV || 'development';
 const PORT = process.env.PORT || 3000;
 
+
 const app = express();
+
+app.use(morgan('dev'));
 
 app.use((req, res, next) => {
     const { method, originalUrl } = req; 
