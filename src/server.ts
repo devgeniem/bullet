24f16df36a7d297e4ce878b1670f0edd7793ee2d
Topi Latva-Salo <topi.latva-salo@geniem.com>
2018-11-29 19:11:20 +0200

    Initial commit. Contains a very basic frame

---
+++ src/server.ts
@@ -0,0 +1,45 @@
+import * as express from 'express';
+
+const enviroment = process.env.NODE_ENV || 'development';
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use((req, res, next) => {
+    const { method, originalUrl } = req; 
+    console.log(`${method} ${originalUrl}`);
+    next();
+});
+
+app.get('/hello', (req, res) => {
+  res.send('Hello, wrold!');
+});
+
+app.get('/info', (req, res) => {
+  res.send('I want to be a real boy!');
+});
+
+app.get('/json', (req, res) => {
+  res.send({
+    sc: 200,
+    json: {
+      ducks: [
+        {
+          name: 'Donald',
+          occupation: 'Postman'
+        },
+        {
+          name: 'Scrooge',
+          occupation: 'Laundering money'
+        }
+      ]
+    }
+  })
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server started at ${new Date()} on port ${PORT}!`);
+});
+
+export default app;
